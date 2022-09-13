4e178f6a09c6c8d12230ce4d7ae64f432f88db9c
clement <hecquet.c59@gmail.com>
2022-09-14 00:46:49 +0200

    Added tests regarding root history

--- test/unit/IncrementalMerkleTree.test.js
+++ test/unit/IncrementalMerkleTree.test.js
@@ -1,13 +1,12 @@
 const { expect } = require('chai');
 const hasherContract = require("../../build/Hasher.json");
 
-const NUMBER_OF_LEVEL = 2;
+const NUMBER_OF_LEVEL = 4;
 const NUMBER_OF_HISTORICAL_ROOTS = 3;
 
 describe('IncrementalMerkleTree contract', () => {
     let IncrementalMerkleTree, incrementalMerkleTree;
     let Hasher, hasher;
-    let addr1, addr2;
 
     beforeEach(async () => {
         Hasher = await ethers.getContractFactory(hasherContract.abi, hasherContract.bytecode);
@@ -36,13 +35,9 @@ describe('IncrementalMerkleTree contract', () => {
         // todo: should initialize the tree correctly
     });
 
-    describe('Basic scenario', () => {
-        // todo: test poseidon hash internal function via https://github.com/frangio/hardhat-exposed
-    });
-
     // todo: rename
-    // in the unit test, i'll test if the event are emitted
-    // I'll test the max number of leaves I can add based on the level
+    // in the unit test, I'll test if the event are emitted -> OK
+    // I'll test the max number of leaves I can add based on the level -> OK
     // I'll test the max number of root history
     describe('Basic scenario', () => {
         it('should add leaves and emit events', async () => {
@@ -67,8 +62,35 @@ describe('IncrementalMerkleTree contract', () => {
             await expect(incrementalMerkleTree.addLeaf(123)).to.be.revertedWith('Merkle tree is full. No more leaves can be added');
         });
 
+        it("should not find a root that isn't in the history", async () => {
+            const isKnownRoot = await incrementalMerkleTree.isKnownRoot("0x123");
+            expect(isKnownRoot).to.be.false;
+        });
+
         it('should keep in the history exactly NUMBER_OF_HISTORICAL_ROOTS roots', async () => {
-            // todo
+            const dummyLeafValue = 0x123;
+            const roots = [];
+            let i;
+            for (i = 0; i < NUMBER_OF_HISTORICAL_ROOTS; i++) {
+                await incrementalMerkleTree.addLeaf(dummyLeafValue);
+                roots.push(await incrementalMerkleTree.getLastRoot());
+            }
+            
+            for (i = 0; i < roots.length; i++) {
+                const isKnownRoot = await incrementalMerkleTree.isKnownRoot(roots[i]);
+                expect(isKnownRoot).to.be.true;
+            }
+
+            await incrementalMerkleTree.addLeaf(dummyLeafValue);
+            roots.push(await incrementalMerkleTree.getLastRoot());
+
+            isKnownRoot = await incrementalMerkleTree.isKnownRoot(roots[0]);
+            expect(isKnownRoot).to.be.false;
+
+            for (i = 1; i < roots.length; i++) {
+                const isKnownRoot = await incrementalMerkleTree.isKnownRoot(roots[i]);
+                expect(isKnownRoot).to.be.true;
+            }
         });
 
 
